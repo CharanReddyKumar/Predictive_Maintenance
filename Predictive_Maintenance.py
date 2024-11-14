cc098ff425323b066aa56ca8c412121590a34027
CharanReddyKumar <charanreddy.kumar@gwmail.gwu.edu>
2024-11-14 14:46:03 -0500

    EDA

--- Predictive_Maintenance.py
+++ Predictive_Maintenance.py
@@ -1,5 +1,9 @@
 #%%
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 # %%
 df = pd.read_csv("ai4i2020.csv")
 # %%
@@ -8,4 +12,48 @@ df. head()
 df.info()
 # %%
 df.describe(include='all')
+# %%
+df.isna().sum()
+# %%
+numerical_cols = df.select_dtypes(include=[np.number])
+categorical_cols = df.select_dtypes(include=["object","category"])
+print("Numerical ", numerical_cols.columns)
+print("Categorical ", categorical_cols.columns)
+# %%
+cor_matrix = df.corr()
+cor_matrix
+# %%
+plt.figure(figsize=(10, 8))
+sns.heatmap(cor_matrix, annot=True, fmt=".2f", cmap="coolwarm", cbar=True)
+plt.title("Correlation Matrix")
+plt.show()
+#%%
+# Plot histograms for all numerical columns
+numerical_cols.hist(bins=20, figsize=(15, 12), layout=(4, 3))
+plt.suptitle("Distribution of Numerical Features")
+plt.show()
+
+# %%
+targets = ['TWF', 'HDF', 'PWF', 'OSF', 'RNF', 'Machine failure']
+for target in targets:
+    sns.countplot(data=df, x=target)
+    plt.title(f"Distribution of {target}")
+    plt.show()
+#%%
+#Relationship between Features and target variables
+for target in targets:
+    for col in numerical_cols.columns:
+        sns.boxplot(data=df, x=target, y=col)
+        plt.title(f"{col} vs {target}")
+        plt.show()
+
+# %%
+#### Feature Engineering
+# Derived features
+df['Temp_Diff'] = df['Process temperature [K]'] - df['Air temperature [K]']
+df['Power'] = df['Torque [Nm]'] * (df['Rotational speed [rpm]'] * 2 * np.pi / 60)
+
+# %%
+
+
 # %%
